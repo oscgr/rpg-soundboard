cefff9a2fa373cce001dbed443fa4d07a713401b
Oscar Grainger <ogr@ehtrace.com>
2026-02-28 22:58:38 +0100

    better loader

--- src/pages/Soundboard.vue
+++ src/pages/Soundboard.vue
@@ -1,15 +1,22 @@
 <template>
   <v-container class="fill-height" fluid>
     <v-row class="w-100 justify-center">
-      <v-col v-for="sound in sounds" :key="sound.id" :cols="cols">
-        <AudioPlayer :sound="sound" />
-      </v-col>
-      <v-col v-if="sounds.length === 0" cols="12">
-        <v-empty-state :icon="mdiVolumeMute" :title="$t('noSound')" />
-      </v-col>
-      <v-col class="text-center" cols="12">
-        <AddSoundDialog />
-      </v-col>
+      <template v-if="loading && sounds.length === 0">
+        <v-col v-for="(_, i) in Array.from({length: 12 / cols})" :key="i" :cols="cols">
+          <v-skeleton-loader height="124px" type="image" />
+        </v-col>
+      </template>
+      <template v-else>
+        <v-col v-for="sound in sounds" :key="sound.id" :cols="cols">
+          <AudioPlayer :sound="sound" />
+        </v-col>
+        <v-col v-if="sounds.length === 0" cols="12">
+          <v-empty-state :icon="mdiVolumeMute" :title="$t('noSound')" />
+        </v-col>
+        <v-col class="text-center" cols="12">
+          <AddSoundDialog />
+        </v-col>
+      </template>
     </v-row>
   </v-container>
 
@@ -25,6 +32,7 @@
   import { db, type Sound } from '@/store/db.ts'
 
   const sounds = ref<Sound[]>([])
+  const loading = ref(true)
   const { mdAndUp, lgAndUp, xlAndUp } = useDisplay()
 
   const subscription = liveQuery(() => db.sounds.orderBy('name').toArray())
@@ -35,6 +43,7 @@
           return b.id - a.id
         return c
       })
+      loading.value = false
     })
 
   const cols = computed(() => {
